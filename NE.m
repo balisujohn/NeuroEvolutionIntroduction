5d26a2f105ee906883ac3a82ce3bc6cb45782363
John Balis <phobossystems@gmail.com>
2018-11-25 16:35:10 -0600

    added advance function

--- NE.m
+++ NE.m
@@ -1,4 +1,5 @@
 
+
 %creates a random adjacency matrix
 Ad = randi([0 1],5,5)
 for i = 1:size(Ad,2)
@@ -7,11 +8,28 @@ end
 
 %creates weight matrix
 W = rand(5,5).* 5 
-s = W .* Ad
-
-activation = 
-
 
 %creates a random threshold vectors
 thresh = rand(5,1).* 10
 
+inputs = [1 ; 1;1 ; 1; 1]
+
+
+
+o = advance(Ad, W, inputs ,thresh)
+    
+    
+    function outputs = advance(Adj , W, i , t)
+    Wa = W .* Adj
+    s = Wa * i
+   % s = atanh(s)
+    o = (s-t)>0
+    outputs = o
+    end
+    
+    
+    
+
+    
+    
+    
