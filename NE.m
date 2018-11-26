34824c44911492f5f09a6ccc0f3a92aaa7e0d3b4
John Balis <phobossystems@gmail.com>
2018-11-26 15:55:40 -0600

    xor evalutation under construction

--- NE.m
+++ NE.m
@@ -2,21 +2,9 @@
     
     
 
-%creates a random adjacency matrix
-Ad = randi([0 1],5,5)
-for i = 1:size(Ad,2)
-Ad(i,i) = 0
-end
 
-%creates weight matrix
-W = rand(5,5).* 5 
 
-%creates a random threshold vectors
-thresh = rand(5,1).* 10
 
-inputs = [1 ; 1 ; 1 ; 1; 1]
-
-[Ad, W, thresh] = mutate(Ad, W, thresh, 2)
 
 
 %simple advance test
@@ -28,14 +16,103 @@ inputs = [1 ; 1 ; 1 ; 1; 1]
 %test3 = connectivityMutation(Ad)
 %test4 = weightMutation(W)
 %test5 = thresholdMutation(thresh)
+%[Ad, W, thresh] = mutate(Ad, W, thresh, 2)
+
+test1 = [0 1 0 ]
+test2 = [0 0 1]
+test3 = combine(test1, test2)
+
+xorTest()
+
+
+    %learningFunctions
+    function result = xorTest()
     
-    function outputs = advance(Adj , W, i , t)
-    Wa = W .* Adj
-    s = Wa * i
-   % s = atanh(s)
-    o = (s-t)>0
-    outputs = o
+    [Adj, W, thresh] = randTopology(10);
+    score = 0;
+    bestScore = 0
+    while score < 4;
+        score = 0;
+        
+        [mAdj,mW,mThresh] = mutate(Adj,W,thresh,10);
+        
+       inputs = [1 1 1 0;
+                 1 0 1 0;
+                 1 1 0 0;
+                 1 0 0 0];
+       %negative one signifies that we aren't checking the output  
+       outputs = [-1 -1 -1 0;
+                  -1 -1 -1 1;
+                  -1 -1 -1 1;
+                  -1 -1 -1 0];
+          
+             
+        for i = 1:size(inputs,1);
+        input = inputs(i,:);
+        goalOutput = outputs(i,:);
+        observedOutput = zeros(1,size(goalOutput,2));
+        testMAdj = mAdj;
+        testMW = mW;
+        testMThresh = mThresh;
+        
+        %running the network
+        for c = 1:10
+        observedOutput = advance(testMAdj,testMW,input, testMThresh);
+        tbi = [inputs(i,:) zeros(1, size(observedOutput',2) - size(inputs,2))];
+        input = combine(tbi,  observedOutput');
+        end
+        
+        for c = 1:size(goalOutput,2)
+         if goalOutput(1,c) > 0 && (goalOutput(1,c) == observedOutput(c,1))
+            score = score +1 ;
+         end
+        end
+        
+        
+        end
+        
+        if score > bestScore
+        Adj = mAdj;
+        W = mW;
+        thresh = mThresh;
+        bestScore = score
+        end
+    
     end
+    
+    result = bestScore;
+    
+    end
+
+    %runtime functions
+    
+    function [Adj, W, thresh] = randTopology(size)
+    %creates a random adjacency matrix
+    Adj = randi([0 1],size,size);
+    for i = 1:size;
+    Adj(i,i) = 0;
+    end
+
+    %creates weight matrix
+    W = rand(size,size).* 5 ;
+
+    %creates a random threshold vectors
+    thresh = rand(size,1).* 10;
+    
+    end
+
+    function outputs = advance(Adj , W, i , t)
+    assert(size(i,1) == 1)
+     if size(i,2) < size(W,2);
+    i = [i zeros(1, size(W,2) - size(i,2))];
+    end
+    Wa = W .* Adj;
+    s = Wa * i';
+   % s = atanh(s);
+    outputs = (s-t)>0;
+    
+    end
+   
     
     %mutation functions
     
@@ -43,7 +120,7 @@ inputs = [1 ; 1 ; 1 ; 1; 1]
     newAdj = Adj;
     newWeights = weights ;
     newThresh = thresh;
-    [newWeights, newAdj, newThresh] = neuronCountMutation(newAdj,newWeights ,newThresh, minSize);
+    [newWeights, newAdj, newThresh] = neuronCountMutation(newAdj, newWeights ,newThresh, minSize);
     newAdj = connectivityMutation(newAdj);
     newWeights = weightMutation(newWeights);
     newThresh = thresholdMutation(newThresh);
@@ -55,12 +132,16 @@ inputs = [1 ; 1 ; 1 ; 1; 1]
     
     if rand(1,1) > .5
         newThresh = [thresh ; rand(1,1)] ;
-        newWeights = [[Adj zeros(size(Adj,1), 1)];zeros(1,size(Adj,1)+1)];
+        newWeights = [[weights zeros(size(weights,1), 1)];zeros(1,size(weights,1)+1)];
         newAdj = [[Adj zeros(size(Adj,1), 1)];zeros(1,size(Adj,1)+1)];
-    elseif size(Adj,1) >minSize
+    elseif size(Adj,1) >minSize 
         newThresh = thresh(1:size(thresh,1)-1, :);
         newWeights = weights(1:size(weights,1)-1,1:size(weights,2)-1);
         newAdj = Adj(1:size(Adj,1)-1,1:size(Adj,2)-1);
+    else
+        newThresh = thresh;
+        newWeights = weights;
+        newAdj = Adj;
     end
     
     end
@@ -71,7 +152,7 @@ inputs = [1 ; 1 ; 1 ; 1; 1]
     output = Adj;
     for i = 1:size(Adj,1)
         for c = 1:size(Adj,1)
-        if rand(1,1) > .9 && i ~= c 
+        if rand(1,1) > .7 && i ~= c 
                output(i,c) = xor(output(i,c),1) ;
         end
         end
@@ -85,7 +166,7 @@ inputs = [1 ; 1 ; 1 ; 1; 1]
     for i = 1:size(weights,1)
         for c = 1:size(weights,1)
         if rand(1,1) > .9 && i ~= c 
-               output(i,c) = output(i,c) + rand(1,1) - .5;
+               output(i,c) = output(i,c) + rand(1,1) - .05;
         end
         end
     end
@@ -104,6 +185,12 @@ inputs = [1 ; 1 ; 1 ; 1; 1]
     
     end
    
+    %utility functions
+    
+    function combined = combine(outputs, inputs)
+    combined = outputs | inputs | [1  zeros(1, size(inputs,2)-1)];
+    
+    end
     
     
     
