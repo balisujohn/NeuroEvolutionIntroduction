723ce9fabb247ffbde27a7d7afe045edd0e0ebb5
JonasKlare <jonasklare4@aol.com>
2018-12-11 21:14:23 -0600

    Progress on popmanagement

--- Problem 1/managePop.m
+++ Problem 1/managePop.m
@@ -8,14 +8,19 @@ function population = managePop(curr, mutVector)
     for i = 1:size(curr,1)
         %TODO implement fitness function
         fitness(i) = fitnessTest(curr(i));
+        breedingChances(i) = ceil(i/3);
     end
     
-    curr = [curr,fitness];
+    curr = [curr,fitness,breedingChances];
 
     %Create 'unfair' breeding - the top performant one gets most mating
     %Sort matrix by the fitness value
     [~,id] = sort(curr(:,4));
     curr = curr(id,:);
+    
+   %TODO Manage population considering score/totalscore * breesize
+   %Then breed, fitness function those, and select top 10 from all. 
+    
     
     
     
